83331d8f95dfc827e6f428b8cc9948bc07384a81
letung3105 <15053897+letung3105@users.noreply.github.com>
2020-11-08 16:25:35 +0700

    game end and restart

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use ggez::conf;
 use ggez::event;
 use ggez::graphics;
+use ggez::mint;
 use ggez::timer;
 use ggez::{Context, GameResult};
-use rand;
 
-const GRID_SIZE: f32 = 25.0;
-const GRID_HEIGHT: i32 = 20;
-const GRID_WIDTH: i32 = 20;
-const FPS: u32 = 6;
+const GRID_SIZE: f32 = 20.0;
+const GRID_HEIGHT: i32 = 40;
+const GRID_WIDTH: i32 = 40;
+const FPS: u32 = 10;
 
 const WINDOW_WIDTH: f32 = GRID_SIZE * GRID_WIDTH as f32;
 const WINDOW_HEIGHT: f32 = GRID_SIZE * GRID_HEIGHT as f32;
 
+/// eukalyptus color palette
+const C_PALETTE: [(u8, u8, u8); 5] = [
+    (155, 167, 166),
+    (129, 140, 135),
+    (118, 143, 133),
+    (50, 60, 56),
+    (27, 29, 27),
+];
+
+const C_RED: (u8, u8, u8) = (255, 0, 0);
+
+/// direction of an object in the grid
 enum Heading {
     Up,
     Down,
@@ -20,91 +32,100 @@ enum Heading {
     Right,
 }
 
+/// manages position of an `Apple` object
 struct Apple {
-    pos: (i32, i32),
+    pos: mint::Point2<i32>,
 }
 
 impl Apple {
-    fn new(grid_width: i32, grid_height: i32) -> Self {
-        Self {
-            pos: (
-                modulo(rand::random::<i32>(), grid_width),
-                modulo(rand::random::<i32>(), grid_height),
-            ),
-        }
+    /// create an Apple at the given grid position
+    fn new(pos: mint::Point2<i32>) -> Self {
+        Self { pos }
     }
 
-    fn update(&mut self, grid_width: i32, grid_height: i32) {
-        self.pos.0 = modulo(rand::random::<i32>(), grid_width);
-        self.pos.1 = modulo(rand::random::<i32>(), grid_height);
+    /// change position of the object
+    fn set_pos(&mut self, pos: mint::Point2<i32>) {
+        self.pos = pos
     }
 
+    /// draw the object to the game window
     fn draw(&self, ctx: &mut Context, grid_size: f32) -> GameResult {
         let apple = graphics::Mesh::new_rectangle(
             ctx,
-            // graphics::DrawMode::Fill(graphics::FillOptions::default()),
             graphics::DrawMode::Fill(graphics::FillOptions::default()),
             graphics::Rect::new(
-                self.pos.0 as f32 * GRID_SIZE,
-                self.pos.1 as f32 * GRID_SIZE,
+                self.pos.x as f32 * grid_size,
+                self.pos.y as f32 * grid_size,
                 GRID_SIZE,
                 GRID_SIZE,
             ),
-            graphics::Color::from_rgb(255, 0, 0),
+            C_RED.into(),
         )?;
         graphics::draw(ctx, &apple, graphics::DrawParam::default())?;
         Ok(())
     }
 }
 
+/// representation of the player in the game
 struct Snake {
     heading: Heading,
-    pos_head: (i32, i32),
-    pos_body: Vec<(i32, i32)>,
+    pos_head: mint::Point2<i32>,
+    pos_body: Vec<mint::Point2<i32>>,
 }
 
 impl Snake {
-    fn new() -> Self {
+    /// create a new snake at a  given grid position
+    fn new(pos_head: mint::Point2<i32>) -> Self {
         Self {
             heading: Heading::Left,
-            pos_head: (0, 0),
+            pos_head,
             pos_body: Vec::new(),
         }
     }
 
-    fn update(&mut self, apple: &Apple, grid_width: i32, grid_height: i32) {
+    /// change the position of all the grids of which the snake is comprised
+    fn update_pos(&mut self, grid_width: i32, grid_height: i32) {
         let mut prev_pos = self.pos_head;
-
         match self.heading {
             Heading::Up => {
-                self.pos_head.1 = modulo(self.pos_head.1 - 1, grid_height);
+                self.pos_head.y = modulo(self.pos_head.y - 1, grid_height);
             }
             Heading::Down => {
-                self.pos_head.1 = modulo(self.pos_head.1 + 1, grid_height);
+                self.pos_head.y = modulo(self.pos_head.y + 1, grid_height);
             }
             Heading::Left => {
-                self.pos_head.0 = modulo(self.pos_head.0 - 1, grid_width);
+                self.pos_head.x = modulo(self.pos_head.x - 1, grid_width);
             }
             Heading::Right => {
-                self.pos_head.0 = modulo(self.pos_head.0 + 1, grid_width);
+                self.pos_head.x = modulo(self.pos_head.x + 1, grid_width);
             }
         }
-
         for pos in self.pos_body.iter_mut() {
-            let tmp = *pos;
-            *pos = prev_pos;
-            prev_pos = tmp;
-        }
-
-        if self.can_eat(apple) {
-            self.pos_body.push(prev_pos);
+            std::mem::swap(pos, &mut prev_pos);
         }
     }
 
+    /// check of the head is overlapped with the apple's position
     fn can_eat(&self, apple: &Apple) -> bool {
         self.pos_head == apple.pos
     }
 
+    /// add a new item to the body of the snake
+    fn grow(&mut self) {
+        self.pos_body.push(self.pos_head);
+    }
+
+    /// check if the snake bites itself
+    fn is_dead(&self) -> bool {
+        for pos_body in &self.pos_body {
+            if *pos_body == self.pos_head {
+                return true;
+            }
+        }
+        false
+    }
+
+    /// draw the snake as rectangles on the screen
     fn draw(&self, ctx: &mut Context, grid_size: f32) -> GameResult {
         for pos_body in &self.pos_body {
             let snake_body = graphics::Mesh::new_rectangle(
@@ -112,55 +133,88 @@ impl Snake {
                 // graphics::DrawMode::Fill(graphics::FillOptions::default()),
                 graphics::DrawMode::Fill(graphics::FillOptions::default()),
                 graphics::Rect::new(
-                    pos_body.0 as f32 * grid_size,
-                    pos_body.1 as f32 * grid_size,
+                    pos_body.x as f32 * grid_size,
+                    pos_body.y as f32 * grid_size,
                     grid_size,
                     grid_size,
                 ),
-                graphics::WHITE,
+                C_PALETTE[0].into(),
             )?;
             graphics::draw(ctx, &snake_body, graphics::DrawParam::default())?;
         }
-
         let snake_head = graphics::Mesh::new_rectangle(
             ctx,
             // graphics::DrawMode::Fill(graphics::FillOptions::default()),
             graphics::DrawMode::Fill(graphics::FillOptions::default()),
             graphics::Rect::new(
-                self.pos_head.0 as f32 * grid_size,
-                self.pos_head.1 as f32 * grid_size,
+                self.pos_head.x as f32 * grid_size,
+                self.pos_head.y as f32 * grid_size,
                 grid_size,
                 grid_size,
             ),
-            graphics::WHITE,
+            C_PALETTE[1].into(),
         )?;
         graphics::draw(ctx, &snake_head, graphics::DrawParam::default())?;
-        ggez::timer::yield_now();
         Ok(())
     }
 }
 
+/// game's state
 struct MainState {
     snake: Snake,
     apple: Apple,
+    is_over: bool,
 }
 
 impl MainState {
+    /// create a new game state
     fn new() -> GameResult<MainState> {
+        let pos_snake_head = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+
+        let mut pos_apple = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+        while pos_apple == pos_snake_head {
+            pos_apple = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+        }
+
         let s = MainState {
-            snake: Snake::new(),
-            apple: Apple::new(GRID_WIDTH, GRID_HEIGHT),
+            snake: Snake::new(pos_snake_head),
+            apple: Apple::new(pos_apple),
+            is_over: false,
         };
         Ok(s)
+    }
+
+    fn restart(&mut self) {
+        let pos_snake_head = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+
+        let mut pos_apple = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+        while pos_apple == pos_snake_head {
+            pos_apple = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+        }
+
+        self.snake = Snake::new(pos_snake_head);
+        self.apple = Apple::new(pos_apple);
+        self.is_over = false;
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         while timer::check_update_time(ctx, FPS) {
-            self.snake.update(&self.apple, GRID_WIDTH, GRID_HEIGHT);
+            if self.is_over {
+                continue;
+            }
+            self.snake.update_pos(GRID_WIDTH, GRID_HEIGHT);
+            if self.snake.is_dead() {
+                self.is_over = true;
+            }
             if self.snake.can_eat(&self.apple) {
-                self.apple.update(GRID_WIDTH, GRID_HEIGHT);
+                let mut new_apple_pos = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+                while new_apple_pos == self.snake.pos_head {
+                    new_apple_pos = rand_point2(GRID_WIDTH, GRID_HEIGHT);
+                }
+                self.apple.set_pos(new_apple_pos);
+                self.snake.grow();
             }
         }
         Ok(())
@@ -190,20 +244,60 @@ impl event::EventHandler for MainState {
                 Heading::Left => {}
                 _ => self.snake.heading = Heading::Right,
             },
-            event::KeyCode::Escape => event::quit(ctx),
+            event::KeyCode::Return => {
+                if self.is_over {
+                    self.restart();
+                }
+            }
+            event::KeyCode::Escape => {
+                if self.is_over {
+                    event::quit(ctx);
+                }
+            }
             _ => println!("Invalid key press!"),
         }
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, graphics::BLACK);
-        self.apple.draw(ctx, GRID_SIZE)?;
-        self.snake.draw(ctx, GRID_SIZE)?;
+        graphics::clear(ctx, C_PALETTE[4].into());
+        if self.is_over {
+            let mut txt_game_over = graphics::Text::new(
+                graphics::TextFragment::new("GAME OVER\n")
+                    .color(C_PALETTE[0].into())
+                    .scale(graphics::Scale::uniform(40.0)),
+            );
+            txt_game_over.add(
+                graphics::TextFragment::new("Press spacebar to restart.\n")
+                    .color(C_PALETTE[0].into())
+                    .scale(graphics::Scale::uniform(32.0)),
+            );
+            txt_game_over.add(
+                graphics::TextFragment::new("Press escape to quit.\n")
+                    .color(C_PALETTE[0].into())
+                    .scale(graphics::Scale::uniform(32.0)),
+            );
+
+            let txt_game_over_dims = txt_game_over.dimensions(ctx);
+            let txt_game_over_dst = mint::Point2 {
+                x: (WINDOW_WIDTH - txt_game_over_dims.0 as f32) / 2.0,
+                y: (WINDOW_HEIGHT - txt_game_over_dims.1 as f32) / 2.0,
+            };
+            graphics::draw(
+                ctx,
+                &txt_game_over,
+                graphics::DrawParam::default().dest(txt_game_over_dst),
+            )?;
+        } else {
+            self.snake.draw(ctx, GRID_SIZE)?;
+            self.apple.draw(ctx, GRID_SIZE)?;
+        }
         graphics::present(ctx)?;
+        ggez::timer::yield_now();
         Ok(())
     }
 }
 
+/// perform the modulo operation on the two given numbers
 fn modulo(x: i32, m: i32) -> i32 {
     let r = x % m;
     if r < 0 {
@@ -213,9 +307,17 @@ fn modulo(x: i32, m: i32) -> i32 {
     }
 }
 
+fn rand_point2(grid_width: i32, grid_height: i32) -> mint::Point2<i32> {
+    mint::Point2 {
+        x: modulo(rand::random::<i32>(), grid_width),
+        y: modulo(rand::random::<i32>(), grid_height),
+    }
+}
+
 fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("01", "Tung L. Vo")
-        .window_mode(conf::WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT));
+        .window_mode(conf::WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT))
+        .window_setup(conf::WindowSetup::default().title("Snaky"));
 
     let (ctx, event_loop) = &mut cb.build()?;
     let state = &mut MainState::new()?;
